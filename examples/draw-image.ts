e5d266d787a8ff0e634ec9e1c3585618126a821a
Jan Fanslau <jan.fanslau@sevenval.com>
2022-09-22 08:22:31 +0200

    cleanup

--- examples/draw-image.ts
+++ examples/draw-image.ts
@@ -4,15 +4,12 @@ import { baseBuffer } from './image-buffer';
 // import * as fs from 'fs';
 // import * as path from 'path';
 
+// todo: check this
+//https://redstapler.co/realtime-video-processing-javascript-tutorial/
 (() => {
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
-    // read bin
-    // const fileContents = fs.readFileSync(path.join(__dirname, 'image.bin'));
-
-    // console.log(fileContents);
     matrix.clear().brightness(80);
-    // const data = Buffer.from(baseBuffer);
     console.log(
       `${baseBuffer.length}  . ${matrix.width() * matrix.height() * 3}`
     );
@@ -22,13 +19,8 @@ import { baseBuffer } from './image-buffer';
         return index % 4 != 0;
       })
     );
-    // const buffer2 = Buffer.of(
-    //   ...baseBuffer.map(() => (Math.random() < 0.1 ? 0xff : 0x00))
-    // );
 
-    // let useBuffer1 = true;
     matrix.afterSync(() => {
-      // useBuffer1 = !useBuffer1;
       matrix.drawBuffer(buffer1);
       setTimeout(() => matrix.sync(), 1);
     });
